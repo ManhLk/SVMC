e68a988bcce7132419c1335a885f8a0709f108ae
= <=>
2020-04-24 11:04:41 +0700

    Link submit: https://www.spoj.com/PTIT/problems/PTIT127F/

--- Xep_sach.cpp
+++ Xep_sach.cpp
@@ -1,25 +1,19 @@
-/*
-Ví dụ n=5 1 3 5 2 4
-Duyệt từ phần từ n xem có ở đáy ko, nếu ko đảo lên trên (bằng cách res+1 )
-Nếu có duyệt phần tử kề trên và làm như trên
-*/
 #include<iostream>
 using namespace std;
-long n, A[300005];
+int n, index, A[300005];
 int main()
 {
-	//freopen("input.txt", "r", stdin);
 	cin >> n;
-	for (int i = 1; i <= n; i++)
+	index = -1;
+	while (true)
 	{
-		cin >> A[i];
+		cin >> A[++index];
+		if (A[index] == n) break;
 	}
-	long book = n, res = 0;
-	for (int i = n; i > 0; i--)
+	for (int i = index; i >= -1; i--)
 	{
-		if (A[i] == book) book--;
-		else res += 1;
+		if (A[i] == n) n--;
 	}
-	cout << res;
+	cout << n;
 	return 0;
 }
